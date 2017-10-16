abc1f7454aaaf004fc22ceb0df790936957a16d4
Ovidiu <ovidiucolnita@gmail.com>
2017-10-16 14:50:03 +0000

    added Edit/Delete buttons

--- models/UIController.js
+++ models/UIController.js
@@ -6,7 +6,9 @@ const UIController = (function () {
         searchType:".select-search-field",
         searchParam: ".search-input",
         searchBtn:".search-button",
-        articleContainer:"#article-container"
+        articleContainer:"#article-container",
+        movieEdit: ".movie-edit",
+        movieDelet: ".movie-delete",
     };
     
     return {
@@ -18,7 +20,7 @@ const UIController = (function () {
         //display the list of movies using insertAdjacentHTML function
         displayMovies:function (movieArr) {
             let html, newHtml; 
-            html = '<article class="movie"><h2><a id="movie-title" href="#">%movieTitle%</a></h2><img src="%posterUrl%"></img><p id="-genremovie">%genre%</p><p id="movie-year">%year%</p><p id="movie-rating">%rating%</p><ul><li class="read-more"><a id="movie-url" href="">Read more</a></li></ul></article>'
+            html = '<article class="col-md-1 movie"><h2><a id="movie-title" href="#">%movieTitle%</a></h2><img src="%posterUrl%"></img><p id="-genremovie">%genre%</p><p id="movie-year">%year%</p><p id="movie-rating">%rating%</p><ul><li class="read-more"><a id="movie-url" href="">Read more</a> <button type="submit" class="movie-edit">Edit</button> <button type="submit" class="movie-delete">Delete</button></li></ul></article>'
             //replace html string with data from backend
             newHtml = html.replace("%movieTitle%", movieArr.Title);
             newHtml= newHtml.replace("%posterUrl%",movieArr.Poster);
