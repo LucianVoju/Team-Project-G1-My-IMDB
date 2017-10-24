dd93e31d70df469d512f600aa1e1ddf30d3a53e3
Mihai Barta <johndoe@example.com>
2017-10-24 16:41:25 +0000

    refactoring

--- models/HomePageUIController.js
+++ models/HomePageUIController.js
@@ -1,12 +1,15 @@
 /*jshint multistr: true */
 //setup the dom strings for further elements acces
-import {getAllMovies,
-	searchMovies,
-	getMovieById,
-	addMovie,
-	deleteMovie,
-	editMovie} from "./MovieController";
- 
+import {
+    getAllMovies,
+    searchMovies,
+    getMovieById,
+    addMovie,
+    deleteMovie,
+    editMovie
+}
+from "./MovieController";
+
 let url = "https://ancient-caverns-16784.herokuapp.com/movies";
 
 let domStrings = {
@@ -26,91 +29,48 @@ function getDomStrings() {
     return domStrings;
 }
 
-//display a movie
-//TODO clone a template instead of this html string and use it
-function displayMovie(movie) {
-    let html, newHtml;
-    html = '<article class="movie">\
-                    <h2>\
-                        <a class="movie-title" href="%url%/movie.html?id=%movieid%">%movieTitle%</a>\
-                    </h2>\
-                    <img class = "poster-url" src="%posterUrl%"></img>\
-                    <p class="-genremovie">%genre%</p>\
-                    <p class="movie-year">%year%</p>\
-                    <p class="movie-rating">%rating%</p>\
-                    <ul class="no-bullet">\
-                        <li class="read-more">\
-                            <a class="movie-url" href="">Read more</a>\
-                        </li>\
-                        <li class="edit-movie">\
-                            <a class="edit-a movie-edit"  data-movieid="%movieid%">Edit Movie</a>\
-                        </li>\
-                        <li class="delete-movie">\
-                            <a class="delete-a movie-delete" data-movieid="%movieid%">Delete Movie</a>\
-                        </li>\
-                    </ul>\
-                </article>';
-    //replace html string with data from backend
-    newHtml = html.replace("%movieTitle%", movie.Title || "Title not awaible");
-    newHtml = newHtml.replace("%posterUrl%", movie.Poster || "https://via.placeholder.com/330x446");
-    newHtml = newHtml.replace("%genre%", movie.Genre);
-    newHtml = newHtml.replace("%year%", movie.Year);
-    newHtml = newHtml.replace("%rating%", movie.imdbRating);
-    newHtml = newHtml.replace("%movieid%", movie._id);
+function displayMovies(movieArray) {
 
-    //insert the HTML into the DOM
-    document.getElementById("article-container").insertAdjacentHTML("afterbegin", newHtml);
-}
-
-
-function displayMovie(movie) {
-    
     var main = document.getElementById("movies");
     var movieElem = document.getElementsByClassName("movie")[0];
 
-    var newMovieElem = movieElem.cloneNode(true);
+    movieArray.forEach(function(movie) {
 
-    var movieTitle = newMovieElem.querySelector("#movie-title");
-    movieTitle.innerHTML = movie.Title;
-    movieTitle.href = url + "/movie.html?id=" + movie._id;
+        var newMovieElem = movieElem.cloneNode(true);
 
-    var posterUrl = newMovieElem.querySelector("#poster-url");
-    posterUrl.innerHTML = movie.Poster;
+        var movieTitle = newMovieElem.querySelector("#movie-title");
+        movieTitle.innerHTML = movie.Title;
+        movieTitle.href = url + "/movie.html?id=" + movie._id;
 
-    var movieGenre = newMovieElem.querySelector("#movie-genre");
-    movieGenre.innerHTML = movie.Genre;
+        var posterUrl = newMovieElem.querySelector("#poster-url");
+        posterUrl.innerHTML = movie.Poster;
 
-    var movieYear = newMovieElem.querySelector("#movie-year");
-    movieYear.innerHTML = movie.Year;
-    
-    var deleteMovieElem = newMovieElem.querySelector("#delete-movie");
-    deleteMovieElem.addEventListener("click", ()=>{
-        deleteMovie(movie._id);
+        var movieGenre = newMovieElem.querySelector("#movie-genre");
+        movieGenre.innerHTML = movie.Genre;
+
+        var movieYear = newMovieElem.querySelector("#movie-year");
+        movieYear.innerHTML = movie.Year;
+
+        var deleteMovieElem = newMovieElem.querySelector("#delete-movie");
+        deleteMovieElem.addEventListener("click", () => {
+            deleteMovie(movie._id);
+        });
+
+        var editMovieElem = newMovieElem.querySelector("#edit_movie");
+        editMovieElem.addEventListener("click", () => {
+            editMovie(movie._id);
+        });
+
+        main.appendChild(newMovieElem);
+
     });
-    
-   var editMovieElem = newMovieElem.querySelector("#edit_movie");
-   deleteMovieElem.addEventListener("click", editMovie);
-    
-    main.appendChild(newMovieElem);
 
 }
 
 //display pagination
-//TODO clone a template instead of this html string and use it
 function displayPagination(pagination) {
 
-    let html, newHtml;
-    html = '<li class="pagination-item">\
-                        <i class="ion-arrow-left-c"></i>\
-                        <p>%current% of %last-page%</p>\
-                        <i class="ion-arrow-right-c"></i>\
-                    </li>'
-    //replace html string with data from backend
-    newHtml = html.replace("%current%", pagination.currentPage);
-    newHtml = newHtml.replace("%last-page%", pagination.numberOfPages);
-    //insert the HTML into the DOM
-
-    document.getElementById("pagination").insertAdjacentHTML("afterbegin", newHtml);
+    document.getElementById("page-number").innerHTML(pagination.currentPage + " of " + pagination.numberOfPages);
 
 }
 //get the users search type and input
@@ -129,7 +89,7 @@ function clearSearchInput() {
 
 export {
     getDomStrings,
-    displayMovie,
+    displayMovies,
     displayPagination,
     getSearchInput,
     clearSearchInput
